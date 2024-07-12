b54b8897f97c78886ca36c50c654a0a457213962
sharadrathod <156213379+sharadrathod@users.noreply.github.com>
2024-07-12 22:52:17 +0530

    a

--- 05_Array.sh
+++ 05_Array.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Array
 myArray=( 1 20 30.5 Hellow "Hey Buddy!" )
-echo "all the values in array  are ${myArray[*]}*
+echo "all the values in array  are ${myArray[*]}"
+
 echo "value in third index is ${myArray[3]}"
