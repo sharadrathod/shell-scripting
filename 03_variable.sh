516205a2cfb7079cdd1649c9762c0899457438d9
sharadrathod <156213379+sharadrathod@users.noreply.github.com>
2024-07-12 22:08:39 +0530

    a

--- 03_variable.sh
+++ 03_variable.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-
+#script to show how to use variable 
 a=10
 name=sharad
 age=21
 
-echo "my name is $name and age is $age"
+echo "my name is $name and age is $age" 
+
+name=piyu
+
+echo "my name is $name"
+
+#var to store the output of a command 
+
+HOSTNAME=#(hostname)
+
+echo "name of this machine is $HOSTNAME"
