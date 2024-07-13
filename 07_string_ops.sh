bcce4335720f52d223ede85a6cb173dcadb0766d
sharadrathod <156213379+sharadrathod@users.noreply.github.com>
2024-07-13 20:45:09 +0530

    a

--- 07_string_ops.sh
+++ 07_string_ops.sh
@@ -3,4 +3,7 @@ myVar="Hey Buddy, How are you?"
 myVarLength=${#myVar}
 echo "Lenghth of myVar is $myVarLength"
 echo "Upper case is ----- ${myVar^^}"
-echo "Lower case is ----  ${myVar,,}"
+echo "Lower case is ----  ${myVar,,}" 
+#To replace a string
+newVar=${myVar/Buddy/Sharad}
+echo "New var is $newVar"
