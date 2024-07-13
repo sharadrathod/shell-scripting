948f00d50396086083379f311e6d45341ef98c72
sharadrathod <156213379+sharadrathod@users.noreply.github.com>
2024-07-13 20:07:58 +0530

    a

--- 06_key-value.sh
+++ 06_key-value.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # how to declare store the key and value 
 declare -A myArray
-myArray=( [name]=Paul [age]=20 [city]=paris )
+myArray=( [name]=sharad [age]=20 [city]=paris )
 echo "Name is ${myArray[name]}"
-echo "Age is ${myarray[age]}"
+echo "Age is ${myArray[age]}"
 echo "City is ${myArray[city]}"
